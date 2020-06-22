753e0a156e9d716539864ae1a574b6e248e79d85
Roman Salo <frs.daddy@gmail.com>
2020-06-22 12:56:31 +0300

    Issue #3153311 by rolki: Add a save method to correct save the revision

--- src/Form/DataPolicyAddForm.php
+++ src/Form/DataPolicyAddForm.php
@@ -6,9 +6,9 @@ use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * asd
+ * Class DataPolicyAddFormю
  *
- * @internal
+ * @package Drupal\data_policy\Form
  */
 class DataPolicyAddForm extends ContentEntityForm {
 
@@ -28,6 +28,40 @@ class DataPolicyAddForm extends ContentEntityForm {
     $form_state->setRedirect('entity.data_policy.collection');
 
     return $this->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $route_name = \Drupal::routeMatch()->getRouteName();
+
+    if ($route_name === 'entity.data_policy.revision_add') {
+      $entity_id = \Drupal::request()->get('entity_id');
+      $this->entity = $this->entityTypeManager->getStorage('data_policy')->load($entity_id);
+
+      $this->entity->setNewRevision(TRUE);
+
+      $active_revision = !empty($form_state->getValue('active_revision'));
+
+      if (!$active_revision) {
+        $this->entity->isDefaultRevision(FALSE);
+      }
+
+      $this->entity->set('name', $form_state->getValue('name'));
+      $this->entity->set('field_description', $form_state->getValue('field_description'));
+      $this->entity->setRevisionCreationTime($this->time->getRequestTime());
+      $this->entity->setRevisionUserId($this->currentUser()->id());
+
+      $this->entity->save();
+      $this->messenger()->addStatus($this->t('Created new revision.'));
+
+      $form_state->setRedirect('entity.data_policy.version_history', ['entity_id' => $entity_id]);
+    }
+    else {
+      return parent::save($form, $form_state);
+    }
+
   }
 
 }
